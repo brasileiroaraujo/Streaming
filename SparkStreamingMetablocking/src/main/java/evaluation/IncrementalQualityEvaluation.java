b6dceae8f098594a8ed1d6144a8ccda565316347
lutibr <lutibr@sis065lap046.staff.ad.uta.fi>
2018-10-05 16:38:04 +0300

    final commit 05.10.18

--- SparkStreamingMetablocking/src/main/java/evaluation/IncrementalQualityEvaluation.java
+++ SparkStreamingMetablocking/src/main/java/evaluation/IncrementalQualityEvaluation.java
@@ -23,8 +23,8 @@ public class IncrementalQualityEvaluation {
 	
 	public static void main(String[] args) {
 		//CHOOSE THE INPUT PATHS
-        String INPUT_PATH_GROUNDTRUTH = "inputs/groundtruth_dblpacm";//"inputs/groundtruth_amazongp";//"inputs/groundtruth_abtbuy";
-        String INPUT_PATH_BLOCKS = "C:\\Users\\lutibr\\Documents\\outputs\\dblp_acm\\";
+        String INPUT_PATH_GROUNDTRUTH = "inputs/groundtruth_abtbuy";//"inputs/groundtruth_amazongp";//"inputs/groundtruth_abtbuy";
+        String INPUT_PATH_BLOCKS = "C:\\Users\\lutibr\\Documents\\outputs\\EDBT\\abt_buy_primewatermark65-550\\";
         
         
     	HashSet<IdDuplicates> groundtruth = null;
